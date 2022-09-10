57495ad6cd0d5aa3a7ef512d593f49754dd60a60
Chesnokov M.A <chesnokov_ma@mail.ru>
2022-09-11 00:12:45 +0500

    Solution task 1

--- seminar1/src/main/java/ru/gb/task1.java
+++ seminar1/src/main/java/ru/gb/task1.java
@@ -10,20 +10,28 @@ public final class task1 {
     public static void main(String[] args) {
         int[] nums = {1,7,3,6,5,6};
         int result = FindIndexEqualSumm(nums);
-        System.out.println(result);
+        if(result<0){
+            System.out.println("Такого индекса не существует");
+        } else {
+            System.out.printf("Индекс равен: %d", result);
+        }
     }
 
    private static int FindIndexEqualSumm(int[] arr) {
     int summ_nums = 0;
     int summ_arr = 0;
     int result = -1;
+
+    // Подсчитываем сумму всех элементов массива
     for (int i = 0; i < arr.length; i++) {
         summ_arr += arr[i];
     }
+
+    // поиск индекса i для которого сумма элементов с индексами < i равна сумме элементов с индексами > i
     for (int i = 0; i < arr.length; i++) {
         summ_nums += arr[i];
         if (((i + 1) < arr.length) && (summ_nums == (summ_arr - summ_nums - arr[i + 1]))) {
-            result = i;
+            result = i + 1;
             break;
         }
     }
