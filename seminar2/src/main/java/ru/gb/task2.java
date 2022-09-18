a2581fafb602e70fc65f74c6bd9c8e129e55a3a8
Chesnokov M.A <chesnokov_ma@mail.ru>
2022-09-18 10:21:38 +0500

    Solution task 1,2,3 seminar2

--- seminar2/src/main/java/ru/gb/task2.java
+++ seminar2/src/main/java/ru/gb/task2.java
@@ -1,9 +1,30 @@
 package ru.gb;
 
+import org.json.JSONObject;
+import java.util.Iterator;
+
 /*
-2. Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки. Если значение null, то параметр не должен попадать в запрос. Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
-Ответ: "select * from students where name = 'Ivanov' and country = 'Russia' and city = 'Moscow'" для приведенного примера
+2. Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса,
+используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
+Если значение null, то параметр не должен попадать в запрос.
+Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+Ответ: "select * from students where name = 'Ivanov' and country = 'Russia' and city = 'Moscow'"
+ для приведенного примера
  */
 public class task2 {
-    
+    public static void main(String[] args) {
+        StringBuilder selectText = new StringBuilder("select * from students where ");
+        StringBuilder whereText = new StringBuilder("");
+        String json = "{name:Ivanov, country:Russia, city:Moscow, age:null}";
+        JSONObject jo = new JSONObject("{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}");
+        String strAnd = "";
+        for (Iterator<String> it = jo.keys(); it.hasNext(); ) {
+            String key = it.next();
+            if (!jo.getString(key).toUpperCase().equals("NULL")) {
+                selectText.append(String.join("", strAnd, key, " = '", jo.getString(key), "'"));
+                strAnd = " and ";
+            }
+        }
+        System.out.println(selectText);
+    }
 }
--- seminar2/src/main/java/ru/gb/task3.java
+++ seminar2/src/main/java/ru/gb/task3.java
@@ -1,19 +1,74 @@
 package ru.gb;
 
-/*
-3**** Дана json строка (можно сохранить в файл и читать из файла)
-[{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
-Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида: Студент [фамилия] получил [оценка] по предмету [предмет].
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+Дана json строка (можно сохранить в файл и читать из файла)
+[{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},
+{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},
+{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+
+Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида:
+ Студент [фамилия] получил [оценка] по предмету [предмет].
 
 Пример вывода:
-
 Студент Иванов получил 5 по предмету Математика.
-
 Студент Петрова получил 4 по предмету Информатика.
-
 Студент Краснов получил 5 по предмету Физика.
  */
 public class task3 {
-    
+    public static void main(String[] args) {
+        String pathFile = System.getProperty("user.dir").concat("\\src\\main\\java\\ru\\gb\\task3.json");
+        System.out.println(pathFile);
+        JSONArray jsonArray = parseJson(pathFile);
+        if (jsonArray.isEmpty()) {
+            System.out.println("Empty json in file");
+        } else {
+            StringBuilder strOut = new StringBuilder();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject item = jsonArray.getJSONObject(i);
+                strOut.append(String.join("","Студент ",
+                    item.getString("фамилия"),
+                    " получил ",
+                    item.getString("оценка"),
+                    " по предмету ",
+                    item.getString("предмет"),
+                    ".\n"));
+            }
+            System.out.println(strOut.toString());
+        }
+    }
+
+    public static JSONArray parseJson(String pathFile) {
+        StringBuilder in = new StringBuilder();
+        try {
+            File jf = new File(pathFile);
+            BufferedReader bufReader = new BufferedReader(new FileReader(jf));
+            try {
+                String str = null;
+                while ((str = bufReader.readLine()) != null) {
+                    in.append(str);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getLocalizedMessage());
+            } finally {
+                bufReader.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (in.length()>0) {
+            JSONArray jsonArray = new JSONArray(in.toString());
+            return jsonArray;
+        } else {
+            return null;
+        }
+    }
 }
